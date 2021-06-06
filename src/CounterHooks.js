126124b12c776e413f5a7dd6a75bcae7eb5338e7
billwishop <willbishop.hello@gmail.com>
2021-06-06 16:02:05 -0500

    Added the ability to toggle the theme with the useContext hook. This addition is on both the class component and the function component.

--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { ThemeContext } from './App'
 
 export default function CounterHooks(props) {
     const [count, setCount] = useState(props.initialCount)
+    const style = useContext(ThemeContext)
     return (
         <div>
-            <button onClick={() => setCount(prevState => prevState - 1)}>-</button>
+            <button style={style} onClick={() => setCount(prevState => prevState - 1)}>-</button>
             <span>{count}</span>
-            <button onClick={() => setCount(prevState => prevState + 1)}>+</button>
+            <button style={style} onClick={() => setCount(prevState => prevState + 1)}>+</button>
         </div>
     )
 }
